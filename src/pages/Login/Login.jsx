ccbedef299b988537eb2f68159362ca414d0d4ce
Matthew Liporada <matthew.liporada@gmail.com>
2026-03-28 15:36:47 +0800

    Login/node server
    Field node server.cjs

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -328,7 +328,7 @@ function Login() {
       value={identifier}
       onChange={handleIdentifierChange}
       // Fix: Either remove "disabled=" or give it a boolean value
-       disabled={false}
+      disabled={true} 
       required
     />
   </div>
